5f56f3bf1971a3b3d078a5d19c0be1c097a216ec
TinoCederholm <42689.@edu.raseko.fi>
2022-03-14 15:03:46 +0200

    lisätty cors

--- todoappi/src/App.js
+++ todoappi/src/App.js
@@ -1,17 +1,29 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 import Todolista from "./TodoLista";
 
-function App() {
-  const data = [
-    {"otsikko": "Ykkönen"},
-    {"otsikko": "Kakkonen"},
-    {"otsikko": "kolmonen"},
-  ];
-  return (
-    <div className="App">
-      <Todolista iteemit={data}/>
-    </div>
-  );
-}
 
-export default App;
+export default class App extends React.Component {
+  state = {
+    iteemit: []
+  }
+
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/api/tehtavat/`)
+      .then(res => {
+        const iteemit = res.data;
+        this.setState({iteemit});
+      })
+  }
+
+  render() {
+    const data = this.state.iteemit;
+    console.log(data)
+    return (
+      <div className="App">
+        <Todolista iteemit={data}/>
+      </div>
+    );
+  }
+}
